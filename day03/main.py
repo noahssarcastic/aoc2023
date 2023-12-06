e8c76894a7d4c425205620598bd06c1bf0ca3586
Noah Iniguez <niniguezg@gmail.com>
2023-12-06 15:04:24 -0600

    Day 3.2

--- day03/main.py
+++ day03/main.py
@@ -1,81 +1,88 @@
-from typing import Generic, List, TypeVar
+from io import TextIOWrapper
+from typing import List
 
 
-T = TypeVar("T")
+class ThreeQueue:
+    N = 3
 
+    def __init__(self) -> None:
+        self.queue: List[str] = ["" for _ in range(self.N)]
 
-class NQueue(Generic[T]):
-    def __init__(self, n: int) -> None:
-        self.queue: List[T | None] = [None for _ in range(n)]
-        self.n = n
-
-    def add(self, x: T) -> None:
-        self.queue[1:] = self.queue[0 : self.n - 1]
+    def add(self, x: str) -> None:
+        self.queue[1:] = self.queue[0 : self.N - 1]
         self.queue[0] = x
 
-    def get(self, i: int) -> T | None:
+    def get(self, i: int) -> str:
         return self.queue[i]
 
 
 def main():
     with open("input.txt", "r", encoding="utf-8") as file:
-        print(get_parts_sum(file))
+        print(parse_file(file))
 
 
-def get_parts_sum(file) -> int:
-    three_queue: NQueue[str] = NQueue(3)
+def parse_file(file: TextIOWrapper) -> int:
+    # we parse lines in three block chunks
+    # the top line is parsed as a chunk of two, itself and the line below
+    # the last line is parsed as a chunk of two, itself and the line above
+    queue = ThreeQueue()
     total = 0
-    while True:
-        raw_line = file.readline()
-        three_queue.add(raw_line.strip())
-        line_to_scan = three_queue.get(1)
-        if line_to_scan is None:
+    for raw_line in file:
+        line_str = raw_line.strip()
+        queue.add(line_str)
+        # if first line skip
+        if line_str == "":
             continue
-        total += scan_line_for_parts(three_queue, line_to_scan)
-        if raw_line == "":
-            break
+        total += sum(find_gear_ratios(queue))
+    # handle last line
+    queue.add("")
+    total += sum(find_gear_ratios(queue))
     return total
 
 
-def scan_line_for_parts(line_queue: NQueue[str], line: str) -> int:
-    cursor_idx = 0
-    total = 0
-    while cursor_idx < len(line):
-        char = line[cursor_idx]
-        if char.isdigit():
-            part_number_len = find_part_number_length(line, cursor_idx)
-            part_number = int(line[cursor_idx : cursor_idx + part_number_len])
-            if is_valid_part(line_queue, cursor_idx, part_number_len):
-                total += part_number
-            cursor_idx = cursor_idx + part_number_len
-        else:
-            cursor_idx += 1
-    return total
-
-
-def find_part_number_length(line: str, start: int) -> int:
-    for i in range(start, len(line)):
-        if not line[i].isdigit():
-            return i - start
-    return len(line) - start
-
-
-def is_valid_part(line_queue: NQueue[str], cursor_idx: int, part_number_len: int):
-    for line in line_queue.queue:
-        if line is None:
+def find_gear_ratios(queue: ThreeQueue) -> List[int]:
+    line = queue.get(1)
+    gear_ratios: List[int] = []
+    for idx, char in enumerate(line):
+        if char != "*":
             continue
-        if any(
-            is_symbol(line[x])
-            for x in range(
-                max(cursor_idx - 1, 0), min(cursor_idx + part_number_len + 1, len(line))
-            )
-        ):
-            return True
-    return False
+        idx_range = range(idx - 1, (idx + 1) + 1)
+        parts = find_part_numbers(queue, idx_range)
+        if len(parts) == 2:
+            gear_ratios.append(parts[0] * parts[1])
+    return gear_ratios
 
 
-def is_symbol(char: str) -> bool:
-    return not char.isdigit() and char != "."
+def find_part_numbers(queue: ThreeQueue, idx_range: range) -> List[int]:
+    # scan above, current, and below lines for part numbers in range
+    parts: List[int] = []
+    for line_to_scan in queue.queue:
+        cursor = 0
+        while cursor < len(line_to_scan):
+            char = line_to_scan[cursor]
+            if not char.isdigit() or cursor not in idx_range:
+                cursor += 1
+                continue
+            start_idx = find_part_number_start(line_to_scan, cursor)
+            end_idx = find_part_number_end(line_to_scan, cursor)
+            part_number = int(line_to_scan[start_idx : end_idx + 1])
+            parts.append(part_number)
+            cursor += end_idx - cursor + 1
+    return parts
+
+
+def find_part_number_start(line: str, idx: int) -> int:
+    for i, char in zip(range(idx, -1, -1), reversed(line[: idx + 1])):
+        if not char.isdigit():
+            return i + 1
+    return 0
+
+
+def find_part_number_end(line: str, idx: int) -> int:
+    for i, char in zip(range(idx, len(line)), line[idx:]):
+        if not char.isdigit():
+            return i - 1
+    return len(line) - 1
 
 
 if __name__ == "__main__":
